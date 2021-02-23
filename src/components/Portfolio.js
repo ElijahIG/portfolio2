7a971c7bb68a0f9ab6c489c8c111bf975a41a666
Elijah Davis <elijahgdavis1999@gmail.com>
2021-02-22 20:08:38 -0800

    Added projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import coded19 from './img/Coded-19.PNG';
+import weatherDashboard from './img/WeatherDashboard.PNG';
+import pokeApp from './img/PokeBase.PNG';
 export default class Porfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -7,13 +10,13 @@ export default class Porfolio extends Component {
       <div className="row">
         <div className="twelve columns collapsed">
           <h1>Check Out Some of My Works.</h1>
-          <img src="img/Coded-19.PNG" alt="Coded-19"></img>
+          <img src={coded19} alt="Coded-19 App"/>
           <a href="https://elijahig.github.io/Coded-19-app/">Coded-19</a>
           <br></br>
-          <img src="img/WeatherDashboard.PNG" alt="Weather Dashboard"></img>
+          <img src={weatherDashboard} alt="Weather Dashboard"/>
           <a href="https://elijahig.github.io/weather-dashboard/">Weather-Dashboard</a>
           <br></br>
-          <img src="img/PokeBase.PNG" alt="PokeBase"></img>
+          <img src={pokeApp} alt="Poke Base"/>
           <a href="https://poke-base-app.herokuapp.com/">Poke-App</a>
         </div>
       </div>
