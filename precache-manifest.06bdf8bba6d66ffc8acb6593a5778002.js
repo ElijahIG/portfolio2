a168a303163b580d64a261fdc187c53c7b91c57b
Elijah Davis <elijahgdavis1999@gmail.com>
2021-02-22 20:12:07 -0800

    Updates

---
+++ precache-manifest.06bdf8bba6d66ffc8acb6593a5778002.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1ada13600d465509378",
+    "url": "/portfolio2/static/css/main.3a8e2229.chunk.css"
+  },
+  {
+    "revision": "b1ada13600d465509378",
+    "url": "/portfolio2/static/js/main.b1ada136.chunk.js"
+  },
+  {
+    "revision": "45bd5e4862a96e41df38",
+    "url": "/portfolio2/static/js/1.45bd5e48.chunk.js"
+  },
+  {
+    "revision": "099cc4b1c2dc80af7879",
+    "url": "/portfolio2/static/js/runtime~main.099cc4b1.js"
+  },
+  {
+    "revision": "391e3ec4e12aa4f98916c0040a405da1",
+    "url": "/portfolio2/static/media/Coded-19.391e3ec4.PNG"
+  },
+  {
+    "revision": "b58b38099c393ec907845d36a48d4261",
+    "url": "/portfolio2/static/media/WeatherDashboard.b58b3809.PNG"
+  },
+  {
+    "revision": "bf76c177601bc8c75aa790e811db51c3",
+    "url": "/portfolio2/static/media/PokeBase.bf76c177.PNG"
+  },
+  {
+    "revision": "73a5e081aca166b9cb7d06772a8a901b",
+    "url": "/portfolio2/index.html"
+  }
+];
